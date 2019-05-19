68373160ec46fe28a80f0b477f44bd1680873fd1
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2019-05-19 17:32:12 +0900

    Update Real-time_inspection_tester.py

--- Real-time_inspection_tester.py
+++ Real-time_inspection_tester.py
@@ -8,37 +8,34 @@ if not os.path.exists("system/Real-time_inspection_test"):
     exit()
 
 while True:
-  if not os.path.exists("setting.xml"):
-    print("""Please select language:
+    if not os.path.exists("setting.xml"):
+        print("""Please select language:
 언어를 선택하세요:
-
 1. English - 영어
 2. 한글 - Korean
 """)
-    language = input("언어를 선택하세요 (1/2) : ")
-    if language==1 or language=="english" or language=="영어":
-        language="en"
-        break
-    elif language==2 or language=="한국어" or language=="한글" or language=="korean" or language=="korea":
-        language="ko"
-        break
+        language = input("언어를 선택하세요 (1/2) : ")
+        if language==1 or language=="english" or language=="영어":
+            language="en"
+            break
+        elif language==2 or language=="한국어" or language=="한글" or language=="korean" or language=="korea":
+            language="ko"
+            break
+        else:
+            pass
     else:
-        pass
-  else:
-      file = open("setting.xml", "r")
-      html=file.read()
-      file.close()
-      soup = BeautifulSoup(html, 'html.parser')
-      language = soup.find("language")
-      filename = soup.find("filename")
-      fileinside = soup.find("fileinside")
-      if not language=="ko" or language=="en":
-          print(language.get_text())
-      print(language.get_text())
-      os.system("pause")
-      break
+        file = open("setting.xml", "r")
+        html=file.read()
+        file.close()
+        soup = BeautifulSoup(html, 'html.parser')
+        language = soup.find("language")
+        filename = soup.find("filename")
+        fileinside = soup.find("fileinside")
+    if language.get_text()=="ko" or language.get_text()=="en":
+        print(language.get_text())
+    os.system("pause")
+    break
     
-
 os.system("title Real-time_inspection_test V.2.0")
 os.system("mode.com con cols=120 lines=40")
 num=0
@@ -131,34 +128,35 @@ while True:
     try:
         file = open("EICAR.TXT", 'r')
     except PermissionError:
-        print("\n\n백신이 가짜 바이러스를 탐지하고 치료하는데 걸린 시간 : 약 [ %s ] 초\n" %num)
+        if language.get_text()=="ko":
+            print("\n\n백신이 미끼파일을 탐지하고 치료하는데 걸린 시간 : 약 [ %s ] 초\n" %num)
+        else:
+            print("\n\nTime taken for the vaccine to detect and treat the decoy file : about [ %s ] seconds.\n" %num)
         os.system("pause")
         break
     except OSError:
-        if language=="ko":
-            d="\n\n백신이 미끼파일을 탐지하고 치료하는데 걸린 시간 : 약 [ %s ] 초\n"%num
+        if language.get_text()=="ko":
+            print("\n\n백신이 미끼파일을 탐지하고 치료하는데 걸린 시간 : 약 [ %s ] 초\n" %num)
         else:
-            d="\n\nTime taken for the vaccine to detect and treat the decoy file : about [ %s ] seconds.\n"%num
-        print(d)
+            print("\n\nTime taken for the vaccine to detect and treat the decoy file : about [ %s ] seconds.\n" %num)
         os.system("pause")
         break
     except FileNotFoundError:
-        if language=="ko":
-            d="\n\n백신이 미끼파일을 탐지하고 치료하는데 걸린 시간 : 약 [ %s ] 초\n"%num
+        if language.get_text()=="ko":
+            print("\n\n백신이 미끼파일을 탐지하고 치료하는데 걸린 시간 : 약 [ %s ] 초\n" %num)
         else:
-            d="\n\nTime taken for the vaccine to detect and treat the decoy file : about [ %s ] seconds.\n"%num
-        print(d)
+            print("\n\nTime taken for the vaccine to detect and treat the decoy file : about [ %s ] seconds.\n" %num)
         os.system("pause")
         break
     eicar=file.read()
     if not eicar=="X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*":
-        print("\n\n백신이 가짜 바이러스를 탐지하고 치료하는데 걸린 시간 : 약 [ %s ] 초\n" %num)
+        print("\n\n백신이 미끼파일을 탐지하고 치료하는데 걸린 시간 : 약 [ %s ] 초\n" %num)
         os.remove("EICAR.TXT")
         os.system("pause")
         break
     if num==10:
         os.remove("EICAR.TXT")
-        print("\n\n백신이 가짜 바이러스를 탐지하고 치료/제거하는데 걸리는 시간이 [ 10 ] 초가 넘었습니다.\n백신의 상태를 다시 한번 확인해 주세요.")
+        print("\n\n백신이 미끼파일을 탐지하고 치료/제거하는데 걸리는 시간이 [ 10 ] 초가 넘었습니다.\n백신의 상태를 다시 한번 확인해 주세요.")
         os.system("pause")
         break
     num=num+1
