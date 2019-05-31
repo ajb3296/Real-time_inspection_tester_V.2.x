1e469db45666b981fbb30cbbc4b6cc6866da2b6a
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2019-05-31 14:24:00 +0900

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,26 +1,40 @@
-# 버전 확인
-    url = "https://newpremium.github.io/version/"
-    r = requests.get(url)
+import os
+import urllib.request
+from bs4 import BeautifulSoup
+import zipfile
+import requests
+import shutil
 
-    soup = BeautifulSoup(r.text, "html.parser")
-    rtit = soup.find("rtit")
-    rtit = rtit.get_text()
-    rtitdownload = soup.find("rtitdownload")
-    url = rtitdownload.get_text()
+file = open("system/version", 'r')
+version=file.read()
+file.close()
 
-    if not rtit=="2.0":
-        print("프로그램을 새 버전으로 업데이트 해야 합니다. 자동으로 업데이트가 진행됩니다.\nYou need to update the program to a new version. The update will proceed automatically.")
-        try:
-            shutil.rmtree('update')
-            except OSError as e:
-        if e.errno == 2:
-            pass
+url = "https://newpremium.github.io/version/"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+rtit = soup.find("rtit")
+rtit = rtit.get_text()
+rtitdownload = soup.find("rtitdownload")
+url = rtitdownload.get_text()
 
-    else:
-        raise
-        urllib.request.urlretrieve(url, "update/master.zip")        
-        zip_ref = zipfile.ZipFile("update/master.zip", 'r')
-        zip_ref.extractall("update")
-        zip_ref.close()
-        os.system("call update\Real-time_inspection_tester_V.%s-master\Real-time_inspection_tester.py")
-        exit()
+# 업데이트 할지 안할지 결정
+if not rtit==version:
+    print("프로그램을 새 버전으로 업데이트 해야 합니다. 자동으로 업데이트가 진행됩니다.\nYou need to update the program to a new version. The update will proceed automatically.")
+    # 폴더 비우기
+    try:
+        shutil.rmtree('update')
+    except FileNotFoundError:
+        os.mkdir("update")
+
+    # 최신버전 다운로드
+    urllib.request.urlretrieve(url, "master.zip")
+
+    # 압축풀기
+    zip_ref = zipfile.ZipFile("master.zip", 'r')
+    zip_ref.extractall("/")
+    zip_ref.close()
+
+    # 불러오기
+    os.system("cls")
+    os.system("call Real-time_inspection_tester_V.%s-master\Real-time_inspection_tester.py" %rtit)
+    exit()
